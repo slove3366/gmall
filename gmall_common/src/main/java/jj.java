76e0efa3a0af8f76512aa646c1c23ad9fd64f396
dyh <dyh@atguigu.com>
2021-02-20 20:19:17 +0800

    haha2

---
+++ gmall_common/src/main/java/jj.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @Author dyhkeep
+ * @Create 2021-02-20 20:13
+ */
+public class jj {
+    public static void main(String[] args) {
+        System.out.println("hot-fix");
+    }
+
+}
